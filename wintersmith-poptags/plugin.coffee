314c7c8a1bcdfaa9e8034d1f9a5ae16e17dcaefd
8legd <git@8legd.com>
2012-10-05 22:39:22 +0100

    started work on built in tags

--- wintersmith-poptags/plugin.coffee
+++ wintersmith-poptags/plugin.coffee
@@ -21,24 +21,21 @@ module.exports = (wintersmith, callback) ->
       else
         try
           tpl = (locals) ->
-            # setup pseudo webpop environment
-            # TODO add more webpop'ness as we need it e.g. in built tags, extensions...
-            # TODO rewrite webprox in coffeescript :)
-            templates = webprox.templates(base)
-
             # set main template (current template)
-            templates.main = contents.toString()
+            templates = { main: contents.toString() }
             read = (name) -> templates[name]
             template = new poptags.Template {
               read: read
               name: 'main'
             }
 
-            # give our scope a webpop flavour
-            locals.contents_tree = locals.contents
-            locals.contents = locals.page
+            # setup pseudo webpop environment
+            # TODO add more webpop'ness as we need it e.g. more built in tags, support for extensions...
+            # TODO rewrite webprox in coffeescript :)
+            webprox.setup(base, templates, locals)
 
             template.render(locals)
+
           callback null, new PopTagsTemplate tpl
         catch error
           callback error
--- wintersmith-poptags/webprox.js
+++ wintersmith-poptags/webprox.js
@@ -14,22 +14,48 @@ var processFilesRecursive = function (dir, processor) {
     });
 }
 
-var _templates;
-exports.templates = function(base) {
-    if(!_templates) {
-        var processor = {
-            base: base,
-            context: 'layouts,includes',
-            templates: {},
-            process: function(file,stat) {
-                if (/.tpl$/i.test(file)) {
-                    var name = file.split(".",1)[0].replace((path.join(this.base,'/')),'');
-                    this.templates[name] = fs.readFileSync(file).toString();
-                }
+var addTemplates = function(base, templates) {
+    var processor = {
+        base: base,
+        context: 'layouts,includes',
+        templates: templates,
+        process: function(file,stat) {
+            if (/.tpl$/i.test(file)) {
+                var name = file.split(".",1)[0].replace((path.join(this.base,'/')),'');
+                this.templates[name] = fs.readFileSync(file).toString();
             }
         }
-        processFilesRecursive(base, processor);
-        _templates = processor.templates;
     }
-    return _templates;
+    processFilesRecursive(base, processor);
+}
+
+var addTags = function(base, locals) {
+    // stylesheets tag
+    locals.stylesheet = {
+        html: { call:
+            function(obj, render_options) {
+                // TODO build timestamp based on last modified of css file
+                var timestamp = new Date().getTime();
+                return "<link rel='stylesheet' href='" +
+                       render_options.name + "?" + timestamp +
+                       "' media='all'/>"
+            }
+        }
+
+    };
+    // TODO javascript tag...etc...
+}
+
+
+exports.setup = function(base, templates, locals) {
+    // add layouts and includes
+    addTemplates(base, templates);
+
+    // add built in tags
+    addTags(base, locals);
+
+    // give our scope a webpop flavour
+    locals.contents_tree = locals.contents;
+    locals.contents = locals.page;
+    locals.contents.body = {html: locals.page.html};
 }
